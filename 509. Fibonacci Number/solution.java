e26c4e7c1f566250c48b9aa8a630c93cf89eb665
Roumo Kundu <65867455+rjzeref@users.noreply.github.com>
2023-07-06 23:59:38 +0530

    Add files via upload

---
+++ 509. Fibonacci Number/solution.java
@@ -0,0 +1,21 @@
+class Solution 
+{
+    public int fib(int n) 
+    {
+        if(n>1)
+        {
+        int a=0;
+        int b=1;
+        int c=a+b;
+        for(int i=2;i<=n;i++)
+        {
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return c;
+        }
+        else
+         return n;
+    }
+}
